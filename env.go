c5dacfa93b5c0f58b3539cc0aba86aef64c38550
sittipongwork <sittipongwork@gmail.com>
2016-12-13 10:40:54 +0700

    add init file

---
+++ env.go
@@ -0,0 +1,15 @@
+package analytic
+
+/**
+* [Environment for Keep Analytics]
+* @const Username 		{string} 		Username of Database
+* @const Password 		{string} 		Password of Database
+* @const MongoIP 		{string} 		MongoDB IP Address
+* @const DBName 			{string} 		Name Database Store
+**/
+const (
+	Username = "root"
+	Password = "rootpassword"
+	MongoIP  = "127.0.0.1"
+	DBName   = "analyticweb"
+)
